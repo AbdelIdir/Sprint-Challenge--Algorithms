44bf54823e4b309900a8f265cde873f8414b1f3c
AbdelI <homeworkoracle@protonmail.com>
2020-04-17 10:28:42 +0200

    updated ansswers for algorithms answers

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -8,13 +8,14 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word, string="th"):
     n1 = len(word)
     n2 = len(string)
+    count=0
 
     if (n1 == 0 or n1 < n2):
         return 0
 
     if (word[0: n2] == string):
         return count_th(word[n2 - 1:],
-                        string) + 1
+                        string) + 1 
 
     return count_th(word[n2 - 1:],
                     string)
